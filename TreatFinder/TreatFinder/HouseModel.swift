2f1d0c6b73992833b77488fae176acc75c00d2e0
Keaton Burleson <keaton.burleson@me.com>
2018-09-29 12:23:17 -0500

    Branch update

---
+++ TreatFinder/TreatFinder/HouseModel.swift
@@ -0,0 +1,17 @@
+//
+//  HouseModel.swift
+//  TreatFinder
+//
+//  Created by Keaton Burleson on 9/29/18.
+//  Copyright © 2018 Keaton Burleson. All rights reserved.
+//
+
+import Foundation
+
+struct HouseModel: Codable{
+    var lat: Double?
+    var long: Double?
+    var name: String?
+    
+    
+}
